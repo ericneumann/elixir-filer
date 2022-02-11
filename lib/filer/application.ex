5cad91728a601126ae47653a5d3841e55cfe0f7e
Eric Neumann <ene1@hey.com>
2022-02-12 09:47:40 +1000

    map-based results

--- lib/filer/application.ex
+++ lib/filer/application.ex
@@ -8,8 +8,7 @@ defmodule Filer.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: Filer.Worker.start_link(arg)
-      # {Filer.Worker, arg}
+      {Filer.Results, %{}}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
--- lib/filer/results.ex
+++ lib/filer/results.ex
@@ -4,31 +4,51 @@ defmodule Filer.Results do
 
   @me __MODULE__
 
-  def start_link(hash_list) do
-    GenServer.start_link(__MODULE__, hash_list, name: __MODULE__)
+  def start_link(hash_map) do
+    GenServer.start_link(__MODULE__, hash_map, name: __MODULE__)
   end
 
-  def init(hash_list) do
-    {:ok, hash_list}
+  def init(hash_map) do
+    {:ok, hash_map}
   end
 
   def get(hash) do
     GenServer.call(@me, { :get, hash} )
   end
 
-  def add( {hash, file} ) do
-    GenServer.cast(@me, { :add, {hash, file} } )
+  def get_duplicates do
+    GenServer.call(@me, :get_duplicates)
   end
 
-  def handle_call( {:get, hash}, _from, hash_list) do
-    hash = String.to_atom(hash)
-    files = Keyword.get_values(hash_list, hash)
-    {:reply, files, hash_list}
+  def add( hash, file ) do
+    GenServer.cast(@me, { :add, hash, file } )
   end
 
-  def handle_cast( { :add, {hash, file}}, hash_list) do
-    hash = String.to_atom(hash)
-    hash_list = Keyword.merge(hash_list, [{hash, file}])
-    { :noreply, hash_list }
+
+  def handle_cast( { :add, hash, file}, hash_map) do
+    hash_map =
+      Map.update(
+        hash_map,
+        hash,
+        [ file ],
+        fn existing ->
+          [ file | existing ]
+        end)
+
+    { :noreply, hash_map }
+  end
+
+  def handle_call( {:get, hash}, _from, hash_map) do
+    files = Map.get(hash_map, hash)
+    {:reply, files, hash_map}
+  end
+
+  def handle_call( :get_duplicates, _from, hash_map) do
+    duplicates = find_duplicate_hashes(hash_map)
+    {:reply, duplicates, hash_map}
+  end
+
+  defp find_duplicate_hashes(hash_map) do
+    Enum.filter(hash_map, fn {_k, v} -> length(v) > 1 end)
   end
 end
